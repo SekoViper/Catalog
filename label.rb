996af64422d7e4058a8e32447cf8e4df397b606f
SekoVper <damehlivingstone1@gmail.com>
2023-03-01 15:11:11 +0000

    Modify code

--- label.rb
+++ label.rb
@@ -2,8 +2,8 @@ require_relative 'item'
 class Label
   attr_accessor :id, :title, :color, :items
 
-  def initialize(title, color: 'white')
-    super(publish_date, archived: false)
+  def initialize(title, color)
+    # super(publish_date, archived: false)
     @id = Random.rand(1...1000)
     @title = title
     @color = color
