092729caf77be639bf24e8ff845957678060edd7
SekoVper <damehlivingstone1@gmail.com>
2023-03-02 12:08:06 +0000

    Add database files for books and labels

---
+++ database/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Books(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY,
+  label_id INT,
+  genre_id INT,
+  author_id INT,
+  publish_date DATE,
+  archived BOOLEAN,
+  FOREIGN KEY(label_id) REFERENCES Label(id),
+  FOREIGN KEY(genre_id) REFERENCES Genre(id),
+  FOREIGN KEY(author_id) REFERENCES Author(id),
+);
+
+CREATE TABLE Label(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY,
+  title VARCHAR(255),
+  color VARCHAR(255)
+)
+
