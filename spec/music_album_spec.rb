b29ec70778723d7bc5c13b24fd4323b525d0021c
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-02 14:32:39 +0300

    fix linters

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -2,11 +2,11 @@ require_relative '../music_album'
 require_relative '../item'
 
 describe MusicAlbum do
-    before :each do
-        @music_album = MusicAlbum.new({})
-    end
+  before :each do
+    @music_album = MusicAlbum.new({})
+  end
 
-    it 'Sets Constructor Arguments' do
-        expect(@music_album.on_spotify).to be_an(Object)
-    end
+  it 'Sets Constructor Arguments' do
+    expect(@music_album.on_spotify).to be_an(Object)
+  end
 end
