21b755f6d10542667992c2032ca546723e9298d4
pius ssekweyama <ssekweyamapius@gmail.com>
2023-03-02 15:13:37 +0300

    add class attributes

--- Author.rb
+++ Author.rb
@@ -1,3 +1,13 @@
-class ClassName
+require_relative 'item'
+class ClassName < Item
+    attr_accessor :id, :last_name
+
+    def initialize(first_name, last_name )
+        super
+        @id = Random.rand(1..1000)
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
     
 end
